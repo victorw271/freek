581ceaa8cca66241353da92b3596c0367984bab4
Freek <46842444+freek1@users.noreply.github.com>
2022-05-01 22:21:36 +0200

    Update model.py

--- model.py
+++ model.py
@@ -75,7 +75,6 @@ tf.random.set_seed(42)
 # Create a model using the Sequential API
 model = tf.keras.Sequential([
     tf.keras.layers.Dense(1), 
-    tf.keras.layers.Dense(1), 
     tf.keras.layers.Dense(1)
     ])
 
@@ -85,7 +84,7 @@ model.compile(loss = tf.keras.losses.mae,
               metrics = ['mae'])
 
 # Fit the model
-model.fit(X_train, y_train, epochs=120)
+model.fit(X_train, y_train, epochs=100)
 
 
 # Make and plot predictions for model_1
